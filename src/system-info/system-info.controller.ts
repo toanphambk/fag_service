49bfc2cd11ec62286061e3a1187d112c229b79f5
toanphambk <toanphambk@gmail.com>
2022-05-01 14:57:44 +0700

    test ipc

--- src/system-info/system-info.controller.ts
+++ src/system-info/system-info.controller.ts
@@ -5,6 +5,10 @@ import { addCarDto } from './dto/carInfo.dto';
 @Controller('system-info')
 export class SystemInfoController {
   constructor(private systemInfoService: SystemInfoService) {}
+  @Get()
+  hello() {
+    return 'this rout work';
+  }
   @Post('addCar')
   addCar(@Body() carInfo: addCarDto): { source: string; description: string } {
     console.log(carInfo);
