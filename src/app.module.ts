2ed8f99131a059780b59071f9f284263b948c167
PHAM Toan <toan.pham@hexagon.com>
2022-05-01 14:06:33 +0700

    test ipc-plc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SystemInfoModule } from './system-info/system-info.module';
 import { PlcCommunicationModule } from './plc-communication/plc-communication.module';
+import { SystemConfigModule } from './system-config/system-config.module';
 
 @Module({
-  imports: [SystemInfoModule, PlcCommunicationModule],
+  imports: [SystemInfoModule, PlcCommunicationModule, SystemConfigModule],
   controllers: [],
   providers: [],
 })
