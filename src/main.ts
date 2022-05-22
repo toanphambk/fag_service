9ded6574b6a1d72bc04da4e58ec7fc1f57d99ec6
toanphambk <toanphambk@gmail.com>
2022-05-23 00:39:12 +0700

    add encoder

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(config.app.port);
+  await app.listen(config.app.port, config.app.ip);
 }
 bootstrap();
