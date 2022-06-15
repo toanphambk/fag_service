03adbe59f793cf9092a349af74ce3e4674d76bfe
toanphambk <toanphambk@gmail.com>
2022-06-16 01:33:42 +0700

    fixed log sequence

--- src/system-log/system-log.service.ts
+++ src/system-log/system-log.service.ts
@@ -1,12 +1,18 @@
 import { ConsoleLogger } from '@nestjs/common';
 import path from 'path';
-import { appendFileSync, readFileSync, writeFileSync } from 'fs';
+import { appendFileSync, existsSync, mkdirSync } from 'fs';
 
 export class SystemLogService extends ConsoleLogger {
+  constructor() {
+    super();
+    if (!existsSync('./dist/log')) {
+      mkdirSync('./dist/log');
+    }
+  }
   error(message: any, stack?: string, context?: string) {
     super.error(message);
     appendFileSync(
-      path.resolve('./log/error.log'),
+      path.resolve('./dist/log/error.log'),
       `[${new Date().toLocaleString()}] ` + message + '\n',
     );
   }
@@ -14,24 +20,24 @@ export class SystemLogService extends ConsoleLogger {
   log(message: any, ...optionalParams: any[]) {
     super.log(message);
     appendFileSync(
-      path.resolve('./log/system-log.log'),
+      path.resolve('./dist/log/system-log.log'),
       `[${new Date().toLocaleString()}] ` + message + '\n',
     );
     if (message.includes('[ ENCODER LOG ]')) {
       appendFileSync(
-        path.resolve('./log/conveyor-log.log'),
+        path.resolve('./dist/log/conveyor-log.log'),
         `[${new Date().toLocaleString()}] ` + message + '\n',
       );
     }
     if (message.includes('[ PLC CONFIG ]')) {
       appendFileSync(
-        path.resolve('./log/plc-config-log.log'),
+        path.resolve('./dist/log/plc-config-log.log'),
         `[${new Date().toLocaleString()}] ` + message + '\n',
       );
     }
     if (message.includes('[ NEW CAR ]')) {
       appendFileSync(
-        path.resolve('./log/car-log.log'),
+        path.resolve('./dist/log/car-log.log'),
         `[${new Date().toLocaleString()}] ` + message + '\n',
       );
     }
